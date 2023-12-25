7a68d4de07d9f38d82ae1e4ed7eb4109aa682a84
Jan Edmark Arieta <edarieta09@gmail.com>
2023-12-25 00:30:28 +0000

    add name

--- play.js
+++ play.js
@@ -1,21 +1,22 @@
-const net = require("net");
+// const net = require("net");
+const connect = require("./client.js")
 
 // establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: 'localhost',
-    port: 50541
-  });
+// const connect = function () {
+//   const conn = net.createConnection({
+//     host: 'localhost',
+//     port: 50541
+//   });
 
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
+//   // interpret incoming data as text
+//   conn.setEncoding("utf8");
 
-  conn.on('data', (data) => {
-    console.log('Server says:', data);
-  });
+//   conn.on('data', (data) => {
+//     console.log('Server says:', data);
+//   });
 
-  return conn;
-};
+//   return conn;
+// };
 
 console.log("Connecting ...");
 connect();
